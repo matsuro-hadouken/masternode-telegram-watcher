dd7ad61920253441646f34bfef5f56f47d5602dd
Matsuro Hadouken <50751381+matsuro-hadouken@users.noreply.github.com>
2019-05-29 13:41:48 +0300

    Update main.sh

--- main.sh
+++ main.sh
@@ -3,15 +3,14 @@ URL="https://api.telegram.org/bot$TOKEN/sendMessage"
 while :
 
         do
-                NODE_IP=$(dogecash-cli getmasternodestatus | grep -E -o "(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)")
+                NODE_IP=$(ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1')
                 MN_STAT=$(dogecash-cli getmasternodestatus | grep status | tr -d 'status":, ')
 
-                if [ $MN_STAT != 4 ]
+                if [ "$MN_STAT" != 4 ]
 
                 then
-                        curl -s -X POST $URL -d chat_id=$CHAT_ID -d text=$NODE_IP" - doesn't feel well"
+                proxychains curl -s -X POST $URL -d chat_id=$CHAT_ID -d text=$NODE_IP" - doesn't feel well"
                 fi
 
                 sleep 1h
         done
-
